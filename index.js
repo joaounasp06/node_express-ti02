4195b7defde647d3ec642a4b7ea5df8df01cc1cd
Nelson <joaonelson08@gmail.com>
2023-11-13 13:20:04 -0300

    recebendo informações do formulário no corpo da requisição

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ const app = express("express")
 
 const caminhoBase = path.join(__dirname, "templates") 
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+
+app.post('cadastrar/salvar', (requisicao, resposta) =>{
+    console.log(requisicao.body)
+})
+
+app.get('/cadastrar', (requisicao, resposta) =>{
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
+}
+)
+
 app.get('/usuarios/:id', (requisicao, resposta) => {
     const id = requisicao.params.id
 
